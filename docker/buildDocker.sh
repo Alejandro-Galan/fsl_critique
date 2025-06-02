459b52344aa7bea6d685961f119b6b931892dc94
Alejandro Galan-Cuenca <dcdfvf@gmail.com>
2025-06-02 19:58:01 +0200

    Functional after Egyptian bug

--- docker/buildDocker.sh
+++
@@ -1,3 +0,0 @@
-name_im="ssl-symbols"
-
-docker build -t $name_im . --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)
--- docker/runDocker_3.sh
+++
@@ -1,8 +0,0 @@
-name_im="ssl-symbols_3"
-cont_im="ssl-symbols"
-gpu=1
-gpu=$1
-
-docker run  -it -u $(id -u):$(id -g) --name $name_im -v $(pwd)/../:/home/user --rm --gpus device=$gpu $cont_im /bin/bash
-#docker run  -it -u $(id -u):$(id -g) --name $name_im --log-driver local -v $(pwd)/logs:/var/log -v $(pwd)/../:/home/user --rm  $name_im /bin/bash
-
