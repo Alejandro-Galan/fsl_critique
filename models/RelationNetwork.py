50f1d7ed84161b8d5d8ff1340de67a316c0a5e96
Alejandro Galan-Cuenca <47591551+Alejandro-Galan@users.noreply.github.com>
2026-03-26 12:45:14 +0100

    Change prototype computation comments

--- models/RelationNetwork.py
+++ models/RelationNetwork.py
@@ -137,6 +137,7 @@ class RelationNetwork(nn.Module):
 
 
 
+    ## As this is for RelationNetworks, we just element-wise sum embeddings of each class
     def compute_prototypes(self, support_features, support_labels):
         """
         Compute class prototypes from support features and labels
@@ -145,19 +146,11 @@ class RelationNetwork(nn.Module):
             support_labels: for each instance in the support set, its label
 
         Returns:
-            for each label of the support set, the average feature vector of instances with this label
+            for each label of the support set, the sum feature vector of instances with this label
         """
 
         n_way = len(torch.unique(support_labels))
         
-        # Prototype i is the mean of all instances of features corresponding to labels == i
-        # return torch.cat(
-        #     [
-        #         support_features[torch.nonzero(support_labels == label)].mean(0)
-        #         for label in range(n_way)
-        #     ]
-        # )
-
         return torch.cat(
             [
                 support_features[torch.nonzero(support_labels == label)].sum(0)
